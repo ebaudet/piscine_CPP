24c16551087cc0c965172b27625f9e2806304ab4
Emilien Baudet <emilien.baudet@gmail.com>
2015-01-15 02:00:28 +0100

    makefile + main de test ex02

--- d07/ex02/main.cpp
+++ d07/ex02/main.cpp
@@ -6,20 +6,24 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 00:19:02 by ebaudet           #+#    #+#             */
-/*   Updated: 2015/01/15 01:12:54 by ebaudet          ###   ########.fr       */
+/*   Updated: 2015/01/15 01:58:36 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int		main(void){
-
+int		main(void) {
 	Array<int> test(2);
+	Array<std::string> teststing(2);
 
 	test[0] = 42;
 	test[1] = 21;
 	int i = test[0];
 	int j = test[1];
 	std::cout << "test[0] : " << i << " , test[1] : " << j << std::endl;
+
+	teststing[0] = "coucou";
+	teststing[1] = "toto";
+	std::cout << "teststing[0] : " << teststing[0] << " , teststing[1] : " << teststing[1] << std::endl;
 	return 0;
 }
